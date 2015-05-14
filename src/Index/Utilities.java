cbdefa48e8cf0c37f73e924fac0997f3ca097ba9
amemon22 <amemon2@gmail.com>
2015-05-14 02:12:30 -0700

    Modified tokenizing function for this project

---
+++ src/Index/Utilities.java
@@ -0,0 +1,22 @@
+package Index;
+
+import java.util.ArrayList;
+
+
+public class Utilities{
+/**
+ * Modified original Utilities class so that it takes in Strings rather 
+ * than files. Used for tokenizing text in files.	
+ */
+	
+	public static ArrayList<String> tokenizeFile(String input) {
+			
+		ArrayList<String> tokens = new ArrayList<String>();
+		for (String str : input.split("\\W")){
+			if (str.length() > 1){
+				tokens.add(str.toLowerCase());
+			}
+		}
+		return tokens;
+	}
+}
--- src/Index/index.java
+++ src/Index/index.java
@@ -1,6 +1,7 @@
 package Index;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -10,19 +11,27 @@ public class index{
 	public static void main(String[] args){
 		Data data = null;
 		ObjectMapper  mapper = new ObjectMapper();
-		try {
-			data = mapper.readValue(new File("c://users/Ahsan Memon/workspaceJava/Project3/FileDump/1.txt"), Data.class);
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		File folder = new File ("c://users/Ahsan Memon/workspaceJava/Project3/FileDump"); 
+		File[] listOfFiles = folder.listFiles();
+		ArrayList<String> tokens = new ArrayList<String>();
+		for (File file : listOfFiles){
+			try {
+				data = mapper.readValue(file, Data.class);
+				tokens = Utilities.tokenizeFile(data.getText());
+				
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(tokens);
 		}
-		System.out.println(data);
+		
 		
 	}
 	
