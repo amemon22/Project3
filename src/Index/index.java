e44c4cb0f6ba3886d8377a7f5b975a24cfa034c7
amemon22 <amemon2@gmail.com>
2015-05-30 22:06:10 -0700

    Added get methods for hashmaps

--- src/Index/index.java
+++ src/Index/index.java
@@ -2,19 +2,16 @@
 //Evan Munemura 43513937
 
 package Index;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
+import java.util.Scanner;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -27,18 +24,44 @@ public class index{
 	//HashMaps for storing our data
 	private static HashMap<String, Integer> term2termid = new HashMap<String, Integer>();
 	private static HashMap<String, Integer> doc2docid = new HashMap<String, Integer>();
+	private static HashMap<Integer, String> docid2doc = new HashMap<Integer, String>();
 	private static HashMap<Integer, ArrayList<Integer>> docid2termid = new HashMap<Integer, ArrayList<Integer>>();
 	private static HashMap<Integer, String> termid2term = new HashMap<Integer, String>();
 	private static HashMap<Integer, HashMap<Integer, Integer>> termid2docidfrequency = new HashMap<Integer, HashMap<Integer, Integer>>();
 	private static HashMap<Integer, HashMap<Integer, Double>> termid2docidtf_idf = new HashMap<Integer, HashMap<Integer, Double>>();
 	
+	//Get methods for obtaining the maps
+	public static HashMap<String, Integer> getTerm2TermID(){
+		return term2termid;
+	}
+	
+	public HashMap<String, Integer> getDoc2DocID(){
+		return doc2docid;
+	}
+	
+	public HashMap<Integer, String> getDocID2Doc(){
+		return docid2doc;
+	}
+	
+	public HashMap<Integer, ArrayList<Integer>> getDocID2TermID(){
+		return docid2termid;
+	}
+	
+	public HashMap<Integer, String> getTermID2Term(){
+		return termid2term;
+	}
+	
+	public HashMap<Integer, HashMap<Integer, Integer>> getTermID2DocIDFrequency(){
+		return termid2docidfrequency;
+	}
+	
+	public HashMap<Integer, HashMap<Integer, Double>> getTermID2DocIDTF_IDF(){
+		return termid2docidtf_idf;
+	}
+	
 	//Comparators for sorting our maps
 	static ValueComparator t2tID = new ValueComparator(term2termid);
 	static ValueComparator d2dID = new ValueComparator(doc2docid);
-	
-	//TreeMaps for our sorted Maps
-	private static TreeMap<String, Integer> sortedterm2termid = new TreeMap<String, Integer>(t2tID);
-	private static TreeMap<String, Integer> sorteddoc2docid = new TreeMap<String, Integer>(d2dID);
 	
 	//Counters for various IDs
 	private static Integer term2termidCounter = 0;
@@ -81,6 +104,8 @@ public class index{
 				termid2docidtf_idf.get(termID).put(docID, tf_idf);
 			}
 		}
+		
+		
 	}
 	
 	//Helper method for adding termIDs to docIDs and term frequency
@@ -122,123 +147,13 @@ public class index{
 		}
 	}
 	
-	//Helper Method to write to a given text to a given filename
-	private static void writeToFile(String text, String filename){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(filename, "UTF-8");
-			writer.write(text);
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+	//Helper method for addid docIDs and docs to the docid2doc Map
+	private static void addDocID2Doc(Integer ID, String URL){
+		if(!docid2doc.containsKey(ID)){
+			docid2doc.put(ID, URL);
 		}
 	}
-	
-	//CSV File Writers
-	//Helper Method to write from a given <String, Integer> hashmap to a given csv file
-	private static void writeTermToIDCSV(TreeMap<String, Integer> sortedterm2termid2, String filename){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(filename, "UTF-8");
-			for(Entry<String, Integer> entry : sortedterm2termid2.entrySet()){
-				writer.write(entry.getKey() + ", " + entry.getValue() + "\r\n");
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//Helper Method to write from a termid2term hashmap to csv file
-	private static void writeIDToTermCSV(HashMap<Integer, String> map, String filename){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(filename, "UTF-8");
-			for(Object id : map.keySet()){
-				writer.write(id + ", " + map.get(id) + "\r\n");
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//Helper Method to write from a given tfidf hashmap to a given csv file
-	private static void writetfidfCSV(HashMap<Integer, HashMap<Integer, Double>> map, String filename){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(filename, "UTF-8");
-			for(Integer id : map.keySet() ){
-				writer.write(id + ", ");
-				HashMap<Integer, Double> innerMap = map.get(id);
-				for(Entry<Integer, Double> entry : innerMap.entrySet()){
-			        writer.write(entry.getKey() + " = " + entry.getValue());
-			        writer.write(", ");
-				}
-				writer.write("\r\n");
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//Helper Method to write from a given termid2docidfrequency hashmap to a given csv file
-	private static void writeFrequencyCSV(HashMap<Integer, HashMap<Integer, Integer>> map, String filename){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(filename, "UTF-8");
-			for(Integer id : map.keySet() ){
-				writer.write(id + ", ");
-				HashMap<Integer, Integer> innerMap = map.get(id);
-				for(Entry<Integer, Integer> entry : innerMap.entrySet()){
-			        writer.write(entry.getKey() + " = " + entry.getValue());
-			        writer.write(", ");
-				}
-				writer.write("\r\n");
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//Helper Method to write from a given doc2termid hashmap to a given csv file
-	private static void writeDoc2TermIDCSV(HashMap<Integer, ArrayList<Integer>> map, String filename){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(filename, "UTF-8");
-			for(Integer id : map.keySet() ){
-				writer.write(id + ", ");
-				ArrayList<Integer> inner = map.get(id);
-				for(int i = 0; i < inner.size(); i++){
-					if(inner.equals(null) || inner.isEmpty()){
-						writer.write("\r\n");
-					}else {
-						writer.write(inner.get(i) + ", ");
-					}
-			        
-				}
-				writer.write("\r\n");
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
-	
+
 	//Returns all the termIDs associated with a list of tokens
 	private static ArrayList<Integer> getTermIDs(ArrayList<String> tokens){
 		ArrayList<Integer> termIDs = new ArrayList<Integer>();
@@ -248,6 +163,23 @@ public class index{
 		return termIDs;
 	}
 	
+	//returns for 5 docIDs in docid2tf_idf given a termID
+	private static ArrayList<Integer> getTopDocIds(String termID){
+		HashMap<Integer, Double> docidtf_idf = termid2docidtf_idf.get(termID);
+		ArrayList<Double> tf_idf = new ArrayList(docidtf_idf.values());
+		Collections.sort(tf_idf);
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		return ret;
+	}
+	
+	//returns top websites given a word
+	private static String searchWord(String input){
+		int termID = term2termid.get(input);
+
+		
+		
+		return "hello";
+	}
 	
 	public static void main(String[] args){
 		
@@ -256,11 +188,8 @@ public class index{
 		ObjectMapper  mapper = new ObjectMapper();
 		//"c://users/Ahsan/Documents/CS121/FileDump" Big Dump
 		//"c://users/Ahsan/Workspace/Java/Project3/FileDump Small Dump"
-<<<<<<< HEAD
-		File folder = new File ("c://users/Ahsan/Documents/CS121/FileDump"); 
-=======
-		File folder = new File ("LargeDump"); 
->>>>>>> origin/master
+		File folder = new File ("c://users/ahsanm/Desktop/FileDump"); 
+
 		File[] listOfFiles = folder.listFiles();
 		ArrayList<String> tokens = new ArrayList<String>();
 		ArrayList<Integer> termIDs = new ArrayList<Integer>();
@@ -287,6 +216,8 @@ public class index{
 				
 				addDoc2DocID(URL,id);
 				
+				addDocID2Doc(id, URL);
+				
 				addDocID2TermID(id, termIDs);
 				
 				addDocID2DocSize(id, tokens.size());
@@ -300,44 +231,17 @@ public class index{
 				e.printStackTrace();
 			}
 		}
-		//These are used to makes the map in order
-		sortedterm2termid.putAll(term2termid);
-		sorteddoc2docid.putAll(doc2docid);
-		
-		//After writing to the file, we clear the maps that we don't need just to speed up the process
-		//Write to CSV Files
-		writeTermToIDCSV(sortedterm2termid, "term2termid.csv");
-		//sortedterm2termid.clear();
-		//term2termid.clear();
-		writeTermToIDCSV(sorteddoc2docid, "doc2docid.csv");
-		//sorteddoc2docid.clear();
-		//doc2docid.clear();
-		writeIDToTermCSV(termid2term, "termid2term.csv");
-		//termid2term.clear();
-		writeFrequencyCSV(termid2docidfrequency, "frequency.csv");
-		//termid2docidfrequency.clear();
-		writetfidfCSV(termid2docidtf_idf, "tfidf.csv");
-		//termid2docidtf_idf.clear();
-		writeDoc2TermIDCSV(docid2termid, "doc2termid.csv");
-		//docid2termid.clear();
-		
-		//Write to Text Files
-		writeToFile(sortedterm2termid.toString(), "term2termid.txt");
-		sortedterm2termid.clear();
-		term2termid.clear();
-		writeToFile(sorteddoc2docid.toString(), "doc2docid.txt");
-		sorteddoc2docid.clear();
-		doc2docid.clear();
-		writeToFile(docid2termid.toString(), "docid2termid.txt");
-		docid2termid.clear();
-		writeToFile(termid2term.toString(), "termid2term.txt");
-		termid2term.clear();
-		writeToFile(termid2docidfrequency.toString(), "termid2docidfrequency.txt");
 		
 		addTermID2DocIDtf_idf();
-		writeToFile(termid2docidtf_idf.toString(), "termid2docidtf_idf.txt");
+
 
 		System.out.println("Done");
+		Scanner reader = new Scanner(System.in);
+		while(true){
+			System.out.println("What would you like to search for? ");
+			String input = reader.nextLine();
+			System.out.println(searchWord(input));
+		}
 	}
 }
 
@@ -355,3 +259,4 @@ class ValueComparator implements Comparator<String> {
         } // returning 0 would merge keys
     }
 }
+
